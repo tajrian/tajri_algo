11148ba0d55f719b059d7d52167ab028f6d5ed74
Tajri <17238731+tajrian@users.noreply.github.com>
2019-11-27 19:34:29 +0600

    Update bit_manipulations_techniques.cpp

--- bit_manipulations_techniques.cpp
+++ bit_manipulations_techniques.cpp
@@ -132,3 +132,27 @@ and 2^i-1 would be the largest power of 2 less or equal to the number
 //x | (1 << n) : Returns the number x with the nth bit set( turn ON(1) if the nth bit of x is off(0))
     
 //toggle: mybits.c ^= 1;
+/*
+You  are  given  two  32-bit  numbers,  N  and  M,  and  two  bit  positions,
+i  and  j  Write  a method to set all bits between i and j in N equal to M (
+e g  , M becomes a substring of N located at i and starting at j)   
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int bit_replace(int n,int m,int i,int j){
+    int max=~0;
+    int left = max-((1<<j)-1);
+    int right = (1<<i)-1;
+    int mask = left | right;
+    return (n&mask)|(m<<i);
+}
+
+int main()
+{
+    printf("%d",bit_replace(1024,21,2,6));
+    //outputs  1108
+    return 0;
+}
