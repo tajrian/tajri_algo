659fef954cf965de4d9c61a1444227e7341925f8
Tajri <17238731+tajrian@users.noreply.github.com>
2019-11-28 19:13:06 +0600

    Update and rename all_errors__I_have_faced to all_errors__I_have_faced.cpp

---
+++ all_errors__I_have_faced.cpp
@@ -0,0 +1,22 @@
+/*This file contains all the erros I have faced so far while coding in c++. 
+These won't run as this is not following correct c++ syntax.
+
+1.the code 
+2.error 
+3.reason
+
+*/
+
+
+1.
+int rank[10];
+
+2.
+---------
+|x| reference to 'rank' is ambiguous.
+
+3.
+------
+since c++11 there is rank struct in library
+
+
